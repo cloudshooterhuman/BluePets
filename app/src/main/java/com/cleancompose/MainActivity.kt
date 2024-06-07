26b6c8a134dcfdf5e99b7149595c92baf1d1c036
abdellahselassi <selassi.abdellah@gmail.com>
2024-06-07 17:13:50 +0200

    fix progress

--- app/src/main/java/com/cleancompose/MainActivity.kt
+++ app/src/main/java/com/cleancompose/MainActivity.kt
@@ -4,22 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -89,12 +95,8 @@ fun PostScreen(
 
             PostUiState.Loading -> {
                 item(span = { GridItemSpan(maxLineSpan) }) {
-                    //LoadingIndicator()
-                    Text(
-                        "Loading ...",
-                        modifier = Modifier
-                            .padding(horizontal = 16.dp)
-                    )
+                    LoadingIndicator(modifier)
+
                 }
             }
 
@@ -108,6 +110,17 @@ fun PostScreen(
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun LoadingIndicator(modifier: Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        CircularProgressIndicator(color = Color.LightGray)
     }
 }
 
