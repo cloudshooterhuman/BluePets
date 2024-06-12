10f51f814155cce90b1cf03d6919465f488ea769
abdellahselassi <selassi.abdellah@gmail.com>
2024-06-12 12:45:48 +0200

    Add toolbar

--- app/src/main/java/com/cleancompose/MainActivity.kt
+++ app/src/main/java/com/cleancompose/MainActivity.kt
@@ -15,8 +15,10 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -43,12 +45,15 @@ import java.nio.charset.StandardCharsets
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(topBar = {
+                    TopAppBar(title = { Text("Pets") })
+                }, modifier = Modifier.fillMaxSize()) { innerPadding ->
                     PostAppNavHost(
                         navController = navController,
                         modifier = Modifier
