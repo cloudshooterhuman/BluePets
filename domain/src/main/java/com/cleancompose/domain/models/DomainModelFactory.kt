2699724ef48c4fad31f624d5dcec7ae71f208a0e
abdellahselassi <selassi.abdellah@gmail.com>
2024-06-19 16:11:48 +0200

    fix unit test.

--- domain/src/main/java/com/cleancompose/domain/models/DomainModelFactory.kt
+++ domain/src/main/java/com/cleancompose/domain/models/DomainModelFactory.kt
@@ -6,6 +6,7 @@ object DomainModelFactory {
     const val POST_TEXT = "POST_TEXT"
     const val POST_IMAGE_URL = "POST_IMAGE_URL"
     const val POST_PUBLISH_DATE = "2020-03-06T23:00:40.972Z"
+    const val POST_PUBLISH_DATE_FORMATED = "6 mars 2020 23:00:40"
 
     const val OWNER_ID = "OWNER_ID"
     const val OWNER_TITLE = "OWNER_TITLE"
@@ -19,7 +20,7 @@ object DomainModelFactory {
         id = id,
         text = POST_TEXT,
         imageUrl = POST_IMAGE_URL,
-        publishDate = POST_PUBLISH_DATE,
+        publishDate = POST_PUBLISH_DATE_FORMATED,
         owner = getDefaultOwnerPreviewModel(),
     )
 
